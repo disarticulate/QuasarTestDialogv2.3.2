aedd9cb08142182b7cc39dcf64d8b0d41f90fc1e
Eric Xanderson <eric@lorefolk.com>
2021-11-14 19:25:48 -0600

    test base

---
+++ src/pages/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex',
+  mounted () {
+    this.$q.dialog({
+      message: `This is Mounted at ${this.$options.name}`
+    })
+    this.$nextTick(() => {
+      this.$q.dialog({
+        message: `This is Mounted after $nextTick at ${this.$options.name}`
+      })
+    })
+  }
+})
+</script>
